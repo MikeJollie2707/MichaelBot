e54ee5ea53cdf78e426586333a16d38c25ae2f43
MikeJollie2707 <bachngo@yahoo.com>
2020-05-26 15:13:03 -0700

    Updated code to 1.3

--- categories/server.py
+++ categories/server.py
@@ -11,18 +11,23 @@ class Server(commands.Cog, name = "Settings", command_attrs = {"cooldown_after_p
         self.bot = bot
         self.emoji = '🛠'
     
-    @commands.command(name = "log-enable")
-    @commands.has_permissions(manage_guild = True)
-    @commands.bot_has_permissions(view_audit_log = True, send_messages = True)
+    @commands.command(aliases = ["log-enable"])
+    @commands.has_guild_permissions(manage_guild = True)
+    @commands.bot_has_permissions(send_messages = True)
+    @commands.bot_has_guild_permissions(view_audit_log = True)
+    @commands.cooldown(rate = 1,  per = 3.0, type = commands.BucketType.guild)
     async def log_enable(self, ctx):
         '''
         Enable logging in your server.
+
+        **Aliases:** `log-enable`
         **Usage:** <prefix>**{command_name}**
         **Example:** {prefix}{command_name}
 
         You need: `Manage Server`.
         I need: `View Audit Log`, `Send Messages`.
         '''
+
         config = methods.get_config(ctx.guild.id)
         if config["STATUS_LOG"] == 1:
             await ctx.send("Logging is already enabled for this server.")
@@ -31,13 +36,17 @@ class Server(commands.Cog, name = "Settings", command_attrs = {"cooldown_after_p
             methods.save_config(config)
             await ctx.send("Logging is enabled for this server. You should setup a log channel.")
 
-    @commands.command(name = "log-setup")
-    @commands.has_permissions(manage_guild = True)
-    @commands.bot_has_permissions(view_audit_log = True, send_messages = True)
+    @commands.command(aliases = ["log-setup"])
+    @commands.has_guild_permissions(manage_guild = True)
+    @commands.bot_has_permissions(send_messages = True)
+    @commands.bot_has_guild_permissions(view_audit_log = True)
+    @commands.cooldown(rate = 1,  per = 3.0, type = commands.BucketType.guild)
     async def log_setup(self, ctx, log : discord.TextChannel = None):
         '''
         Set or view a log channel in your server.
         If this command is invoked but you haven't enabled logging, it'll automatically be enabled.
+
+        **Aliases:** `log-setup`
         **Usage:** <prefix>**{command_name}** [text channel mention/ID/name]
         **Example 1:** {prefix}{command_name} 649111117204815883
         **Example 2:** {prefix}{command_name} a-random-log-channel
@@ -46,6 +55,7 @@ class Server(commands.Cog, name = "Settings", command_attrs = {"cooldown_after_p
         You need: `Manage Server`.
         I need: `View Audit Log`, `Send Messages`.
         '''
+
         config = methods.get_config(ctx.guild.id)
         if log is None:
             await ctx.send("Current logging channel ID: `%d`" % config["LOG_CHANNEL"])
@@ -54,22 +64,25 @@ class Server(commands.Cog, name = "Settings", command_attrs = {"cooldown_after_p
             config["LOG_CHANNEL"] = log.id
             methods.save_config(config)
             
-            embed = discord.Embed(
+            embed = methods.get_default_embed(
                 title = "Logging Enabled", 
                 description = "This is now a logging channel.", 
-                color = discord.Color.green()
-            )
-            embed.set_author(
+                color = discord.Color.green(),
+                timestamp = datetime.datetime.utcnow()
+            ).set_author(
                 name = self.bot.user.name, 
-                icon_url = self.bot.user.avatar_url\
+                icon_url = self.bot.user.avatar_url
+            ).set_footer(
+                text = "Set by %s" % ctx.author.name,
+                icon_url = ctx.author.avatar_url
             )
-            embed.timestamp = datetime.datetime.utcnow()
 
             await log.send(embed = embed)
     
-    @commands.command(name = "log-disable")
-    @commands.has_permissions(manage_guild = True)
+    @commands.command(aliases = ["log-disable"])
+    @commands.has_guild_permissions(manage_guild = True)
     @commands.bot_has_permissions(send_messages = True)
+    @commands.cooldown(rate = 1,  per = 3.0, type = commands.BucketType.guild)
     async def log_disable(self, ctx):
         '''
         Disable logging in your server.
@@ -79,12 +92,16 @@ class Server(commands.Cog, name = "Settings", command_attrs = {"cooldown_after_p
         You need: `Manage Server`.
         I need: `Send Messages`.
         '''
+
         config = methods.get_config(ctx.guild.id)
         config["STATUS_LOG"] = 0
         methods.save_config(config)
         await ctx.send("Logging is disabled for this server.")
 
-    @commands.command(name = "welcome-enable")
+    @commands.command(aliases = ["welcome-enable"])
+    @commands.has_guild_permissions(manage_guild = True)
+    @commands.bot_has_permissions(send_messages = True)
+    @commands.cooldown(rate = 1,  per = 3.0, type = commands.BucketType.guild)
     async def welcome_enable(self, ctx):
         '''
         Enable welcoming new members in your server.
@@ -102,11 +119,16 @@ class Server(commands.Cog, name = "Settings", command_attrs = {"cooldown_after_p
             methods.save_config(config)
             await ctx.send("Welcoming is enabled for this server. You should setup the welcome channel and message.")
     
-    @commands.command(name = "welcome-setup")
+    @commands.command(aliases = ["welcome-setup"])
+    @commands.has_guild_permissions(manage_guild = True)
+    @commands.bot_has_permissions(send_messages = True)
+    @commands.cooldown(rate = 1,  per = 3.0, type = commands.BucketType.guild)
     async def welcome_setup(self, ctx, welcome_chan : discord.TextChannel = None, *, welcome_text : str = ""):
         '''
         Set or view the welcome channel and message in your server.
         If this command is invoked but you haven't enabled welcoming, it'll automatically be enabled.
+
+        **Aliases:** `welcome-setup`
         **Usage:** <prefix>**{command_name}** [text channel mention/ID/name] [welcome text]
         **Argument:** `[user.mention]`, `[user.name]`, `[guild.name]`, `[guild.count]`
         **Example 1:** {prefix}{command_name} 644336991135072261 Welcome [user.mention] to [guild.name]!
@@ -117,21 +139,27 @@ class Server(commands.Cog, name = "Settings", command_attrs = {"cooldown_after_p
         You need: `Manage Server`.
         I need: `Send Messages`.
         '''
+
         config = methods.get_config(ctx.guild.id)
         if welcome_chan is None:
-            await ctx.send("Current welcome channel ID: `%d`"  % config["WELCOME_CHANNEL"])
-            await ctx.send("Current welcome message: ", embed = discord.Embed(description = config["WELCOME_TEXT"]))
+            await ctx.send(f"Current welcome channel ID: `{config['WELCOME_CHANNEL']}`\nCurrent welcome message: ", embed = discord.Embed(description = config["WELCOME_TEXT"]))
         else:
             config["STATUS_WELCOME"] = 1
-            config["WELCOME_CHANNEL"] = welcome_chan.id
-            await ctx.send("Channel <#%d> is now a welcome channel." % welcome_chan.id)
 
             if welcome_text == "":
                 welcome_text = "Hello [user.mention]! Welcome to **[guild.name]**! You're the [guild.count]th member in this server! Enjoy the fun!!! :tada:"
-
             config["WELCOME_TEXT"] = welcome_text
+
+            try:
+                await welcome_chan.send("Your welcome message is: %s" % welcome_text)
+            except discord.Forbidden:
+                await ctx.send(f"I can't send message to {welcome_chan.mention}!")
+            else:
+                config["WELCOME_CHANNEL"] = welcome_chan.id
+                await ctx.send("Channel %s is now a welcome channel." % welcome_chan.mention)
+
             methods.save_config(config)
-            await welcome_chan.send("Your welcome message is: %s" % welcome_text)
+
 
     @commands.Cog.listener("on_member_join")
     async def welcome_new_member(self, member):
@@ -139,6 +167,8 @@ class Server(commands.Cog, name = "Settings", command_attrs = {"cooldown_after_p
         if config["ERROR"] == 0 and config["STATUS_WELCOME"] == 1 and config["WELCOME_CHANNEL"] != 0:
             welcome_channel = self.bot.get_channel(config["WELCOME_CHANNEL"])
             welcome_text = config["WELCOME_TEXT"]
+
+            # We don't use f-string here because it'll raise attribute errors which are annoying.
             welcome_text = welcome_text.replace("[user.mention]", "<@%d>" % member.id)
             welcome_text = welcome_text.replace("[user.name]", str(member))
             welcome_text = welcome_text.replace("[guild.name]", str(member.guild))
@@ -148,16 +178,22 @@ class Server(commands.Cog, name = "Settings", command_attrs = {"cooldown_after_p
         else:
             return
 
-    @commands.command(name = "welcome-disable")
+    @commands.command(aliases = ["welcome-disable"])
+    @commands.has_guild_permissions(manage_guild = True)
+    @commands.bot_has_permissions(send_messages = True)
+    @commands.cooldown(rate = 1,  per = 3.0, type = commands.BucketType.guild)
     async def welcome_disable(self, ctx):
         '''
         Disable welcoming in your server.
+
+        **Aliases:** `welcome-disable`
         **Usage:** <prefix>**{command_name}**
         **Example:** {prefix}{command_name}
 
         You need: `Manage Server`.
         I need: `Send Messages`.
         '''
+        
         config = methods.get_config(ctx.guild.id)
         config["STATUS_WELCOME"] = 0
         methods.save_config(config)
