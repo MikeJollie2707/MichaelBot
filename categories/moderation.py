458f3459e3dfbdb8c9ac2fb4cc0794784079fa77
MikeJollie2707 <bachngo@yahoo.com>
2020-06-07 23:16:12 -0700

    Sorted commands

--- categories/moderation.py
+++ categories/moderation.py
@@ -11,7 +11,95 @@ class Moderation(commands.Cog, command_attrs = {"cooldown_after_parsing" : True}
     def __init__(self, bot):
         self.bot = bot
         self.emoji = '🔨'
-    
+
+    @commands.command()
+    @commands.has_guild_permissions(ban_members = True)
+    @commands.bot_has_permissions(send_messages = True)
+    @commands.bot_has_guild_permissions(ban_members = True)
+    @commands.cooldown(rate = 2, per = 5.0, type = commands.BucketType.guild)
+    async def ban(self, ctx, user : discord.Member, *, reason = None):
+        '''
+        Ban a member __in__ the server.
+
+        **Usage:** <prefix>**{command_name}** <name/ID/nickname/mention> [reason]
+        **Cooldown:** 5 seconds per 2 uses (guild).
+        **Example 1:** {prefix}{command_name} MikeJollie Spam too much
+        **Example 2:** {prefix}{command_name} @MikeJollie Stop spamming!
+        
+        **You need:** `Ban Members`.
+        **I need:** `Ban Members`, `Send Messages`.
+        '''
+
+        guild = ctx.guild
+        victim_name = str(user)
+        if reason == None:
+            reason = "`None provided`"
+        try:
+            await guild.ban(user, reason = reason)
+        except discord.Forbidden as f:
+            await ctx.send("I cannot ban someone that's higher than me!")
+        else:
+            embed = methods.get_default_embed(
+                title = "Member Banned",
+                description = '''
+                        **User `%s` has been banned from this server.** :hammer:
+                        **Reason:** %s
+                ''' % (victim_name, reason),
+                color = 0x000000,
+                timestamp = datetime.utcnow()
+            ).set_footer(
+                text = f"Banned by {ctx.author.name}",
+                icon_url = ctx.author.avatar_url
+            )
+
+            await ctx.send(embed = embed)
+    @ban.error
+    async def ban_error(self, ctx, error):
+        if isinstance(error, commands.BadArgument):
+            await ctx.send("I cannot ban someone that's not in the guild normally. I need the power of `%shackban` to ban." % ctx.prefix)
+
+    @commands.command()
+    @commands.has_guild_permissions(ban_members = True)
+    @commands.bot_has_permissions(send_messages = True)
+    @commands.bot_has_guild_permissions(ban_members = True)
+    @commands.cooldown(rate = 2, per = 5.0, type = commands.BucketType.guild)
+    async def hackban(self, ctx, id : int, *, reason = None):
+        '''
+        Ban a user __outside__ the server.
+
+        **Usage:** <prefix>**{command_name}** <ID> [reason]
+        **Cooldown:** 5 seconds per 2 uses (guild).
+        **Example:** {prefix}{command_name} 472832990012243969 Develope a bot
+
+        **You need:** `Ban Members`.
+        **I need:** `Ban Members`, `Send Messages`.
+        '''
+
+        guild = ctx.guild
+        if reason == None:
+            reason = "`None provided`"
+        
+        try:
+            await guild.ban(discord.Object(id = id), reason = reason)
+        except discord.HTTPException:
+            await ctx.send("I can't ban this person. The most common one would be your id is wrong.")
+            return
+        else:
+            embed = methods.get_default_embed(
+                title = "User Banned",
+                description = '''
+                        **User `%d` has been banned from this server.** :hammer:
+                        **Reason:** %s
+                ''' % (id, reason),
+                color = 0x000000,
+                timestamp = datetime.utcnow()
+            ).set_footer(
+                text = f"Banned by {ctx.author.name}",
+                icon_url = ctx.author.avatar_url
+            )
+
+            await ctx.send(embed = embed)
+
     @commands.command()
     @commands.has_guild_permissions(kick_members = True)
     @commands.bot_has_permissions(send_messages = True)
@@ -59,136 +147,8 @@ class Moderation(commands.Cog, command_attrs = {"cooldown_after_parsing" : True}
     async def kick_error(self, ctx, error):
         if isinstance(error, commands.BadArgument):
             await ctx.send("I cannot kick someone that's not in the guild! If you want someone not to join your guild, use `%shackban`." % ctx.prefix)
-
-    @commands.command()
-    @commands.has_guild_permissions(ban_members = True)
-    @commands.bot_has_permissions(send_messages = True)
-    @commands.bot_has_guild_permissions(ban_members = True)
-    @commands.cooldown(rate = 2, per = 5.0, type = commands.BucketType.guild)
-    async def ban(self, ctx, user : discord.Member, *, reason = None):
-        '''
-        Ban a member __in__ the server.
-
-        **Usage:** <prefix>**{command_name}** <name/ID/nickname/mention> [reason]
-        **Cooldown:** 5 seconds per 2 uses (guild).
-        **Example 1:** {prefix}{command_name} MikeJollie Spam too much
-        **Example 2:** {prefix}{command_name} @MikeJollie Stop spamming!
-        
-        **You need:** `Ban Members`.
-        **I need:** `Ban Members`, `Send Messages`.
-        '''
-
-        guild = ctx.guild
-        victim_name = str(user)
-        if reason == None:
-            reason = "`None provided`"
-        try:
-            await guild.ban(user, reason = reason)
-        except discord.Forbidden as f:
-            await ctx.send("I cannot ban someone that's higher than me!")
-        else:
-            embed = methods.get_default_embed(
-                title = "Member Banned",
-                description = '''
-                        **User `%s` has been banned from this server.** :hammer:
-                        **Reason:** %s
-                ''' % (victim_name, reason),
-                color = 0x000000,
-                timestamp = datetime.utcnow()
-            ).set_footer(
-                text = f"Banned by {ctx.author.name}",
-                icon_url = ctx.author.avatar_url
-            )
-
-            await ctx.send(embed = embed)
-    @ban.error
-    async def ban_error(self, ctx, error):
-        if isinstance(error, commands.BadArgument):
-            await ctx.send("I cannot ban someone that's not in the guild normally. I need the power of `%shackban` to ban." % ctx.prefix)
     
-    @commands.command()
-    @commands.has_guild_permissions(ban_members = True)
-    @commands.bot_has_permissions(send_messages = True)
-    @commands.bot_has_guild_permissions(ban_members = True)
-    @commands.cooldown(rate = 2, per = 5.0, type = commands.BucketType.guild)
-    async def hackban(self, ctx, id : int, *, reason = None):
-        '''
-        Ban a user __outside__ the server.
-
-        **Usage:** <prefix>**{command_name}** <ID> [reason]
-        **Cooldown:** 5 seconds per 2 uses (guild).
-        **Example:** {prefix}{command_name} 472832990012243969 Develope a bot
-
-        **You need:** `Ban Members`.
-        **I need:** `Ban Members`, `Send Messages`.
-        '''
-
-        guild = ctx.guild
-        if reason == None:
-            reason = "`None provided`"
-        
-        try:
-            await guild.ban(discord.Object(id = id), reason = reason)
-        except discord.HTTPException as httperr:
-            await ctx.send("I can't ban this person. The most common one would be your id is wrong.")
-            return
-        else:
-            embed = methods.get_default_embed(
-                title = "User Banned",
-                description = '''
-                        **User `%d` has been banned from this server.** :hammer:
-                        **Reason:** %s
-                ''' % (id, reason),
-                color = 0x000000,
-                timestamp = datetime.utcnow()
-            ).set_footer(
-                text = f"Banned by {ctx.author.name}",
-                icon_url = ctx.author.avatar_url
-            )
-
-            await ctx.send(embed = embed)
-
-    @commands.command()
-    @commands.has_guild_permissions(ban_members = True)
-    @commands.bot_has_permissions(send_messages = True)
-    @commands.bot_has_guild_permissions(ban_members = True)
-    @commands.cooldown(rate = 2, per = 5.0, type = commands.BucketType.guild)
-    async def unban(self, ctx, id : int, *, reason = None):
-        '''
-        Unban a user.
-
-        **Usage:** <prefix>**{command_name}** <ID> [reason]
-        **Cooldown:** 5 seconds per 2 uses (guild).
-        **Example:** {prefix}{command_name} 472832990012243969 You've redeemed your goodness.
-
-        **You need:** `Ban Members`.
-        **I need:** `Ban Members`, `Send Messages`.
-        '''
-
-        guild = ctx.guild
-
-        try:
-            await guild.unban(discord.Object(id = id), reason = reason)
-        except discord.HTTPException:
-            await ctx.send("The ban hammer is too heavy! Make sure the id is correct, and that the user is already banned.")
-            return
-        else:
-            embed = methods.get_default_embed(
-                title = "Member Unbanned",
-                description = '''
-                        **User `%d` has been banned from this server.** :hammer:
-                        **Reason:** %s
-                ''' % (id, reason),
-                color = 0x000000,
-                timestamp = datetime.utcnow()
-            ).set_footer(
-                text = f"Unbanned by {ctx.author.name}",
-                icon_url = ctx.author.avatar_url
-            )
-            
-            await ctx.send(embed = embed)
-
-    @commands.command(hidden = True, disabled = True)
+    @commands.command(hidden = True, enabled = True)
     @commands.has_permissions(kick_members = True)
     @commands.bot_has_guild_permissions(kick_members = True)
     @commands.cooldown(1, 5.0, commands.BucketType.guild)
@@ -262,6 +222,47 @@ class Moderation(commands.Cog, command_attrs = {"cooldown_after_parsing" : True}
             await ctx.send("Failed channels: %s" % failed_channels)
 
         # Perform DB actions here
+
+    @commands.command()
+    @commands.has_guild_permissions(ban_members = True)
+    @commands.bot_has_permissions(send_messages = True)
+    @commands.bot_has_guild_permissions(ban_members = True)
+    @commands.cooldown(rate = 2, per = 5.0, type = commands.BucketType.guild)
+    async def unban(self, ctx, id : int, *, reason = None):
+        '''
+        Unban a user.
+
+        **Usage:** <prefix>**{command_name}** <ID> [reason]
+        **Cooldown:** 5 seconds per 2 uses (guild).
+        **Example:** {prefix}{command_name} 472832990012243969 You've redeemed your goodness.
+
+        **You need:** `Ban Members`.
+        **I need:** `Ban Members`, `Send Messages`.
+        '''
+
+        guild = ctx.guild
+
+        try:
+            await guild.unban(discord.Object(id = id), reason = reason)
+        except discord.HTTPException:
+            await ctx.send("The ban hammer is too heavy! Make sure the id is correct, and that the user is already banned.")
+            return
+        else:
+            embed = methods.get_default_embed(
+                title = "Member Unbanned",
+                description = '''
+                        **User `%d` has been banned from this server.** :hammer:
+                        **Reason:** %s
+                ''' % (id, reason),
+                color = 0x000000,
+                timestamp = datetime.utcnow()
+            ).set_footer(
+                text = f"Unbanned by {ctx.author.name}",
+                icon_url = ctx.author.avatar_url
+            )
+            
+            await ctx.send(embed = embed)
+
 
 def setup(bot):
     bot.add_cog(Moderation(bot))
