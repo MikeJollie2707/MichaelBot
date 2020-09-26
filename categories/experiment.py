4e59860abec566607dcd5eff4151978d1a69df45
MikeJollie2707 <bachngo@yahoo.com>
2020-09-25 22:35:54 -0700

    Unignored experiment

--- categories/experiment.py
+++ categories/experiment.py
@@ -1,14 +1,20 @@
 import discord
 from discord.ext import commands
 
+from categories.utilities.db import DB
+
 # This category is for testing purpose.
 class Experiment(commands.Cog):
     def __init__(self, bot):
         self.bot = bot
     
-    @commands.command
-    async def testinitDB(self, ctx):
-        pass    
+    @commands.command()
+    async def drop(self, ctx):
+        await DB.drop_all_table(self.bot)
+    @commands.command()
+    async def create(self, ctx):
+        await DB.init_db(self.bot)
+
     
 
 
