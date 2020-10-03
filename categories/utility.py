12262f09095982d05660ed489d0bbd6044bf9178
MikeJollie2707 <bachngo@yahoo.com>
2020-10-02 21:46:40 -0700

    Upgraded konachan, again

--- categories/utility.py
+++ categories/utility.py
@@ -252,99 +252,148 @@ class Utility(commands.Cog, command_attrs = {"cooldown_after_parsing" : True}):
         else:
             await ctx.send(f"{target} is `{percent_gay}%` gay :rainbow_flag:.")
 
-    @commands.command(hidden = True)
+    @commands.group(invoke_without_command = True)
     @commands.is_nsfw()
     @commands.bot_has_permissions(send_messages = True)
-    @commands.cooldown(rate = 1, per = 3.0, type = commands.BucketType.member)
-    async def konachan(self, ctx, *, tags : str = ""):
+    @commands.cooldown(rate = 1, per = 5.0, type = commands.BucketType.member)
+    async def konachan(self, ctx, img_type : str, *, tags : str = ""):
         '''
-        Send a **safe** picture from konachan**.net**
-        **Disclaimer:** We, the bot developers team, hold **ZERO** responsibilities if FBI visit you.
+        Send a picture from konachan API.
 
-        This will run a search on `konachan.net` and return an image chosen deemed to be safe.
+        This will run a search on either `konachan.net` (mostly safe images) or `konachan.com` and return an image chosen deemed to be safe.
 
         - If not provided the tags, it'll search a random image.
-        - If provided with tag(s), it'll search an image having all the tags. The tags **must be exactly the same** as it appears in `konachan.net`. 
+        - If provided with tag(s), it'll search an image having all the tags. The tags **must be exactly the same** as it appears in `konachan.net`/`konachan.com`. 
         
-        Visit [this page](https://konachan.net/tag) to see all the tags.
+        Visit [this page](https://konachan.net/tag) to see all the tags, or you can use `{prefix}konachan tags` to see some popular tags.
 
-        **Usage:** <prefix>**{command_name}** [exact tags separated by space]
-        **Cooldown:** 3 seconds per 1 use (member)
-        **Example 1:** {prefix}{command_name} blush long_hair
-        **Example 2:** {prefix}{command_name}
+        **Usage:** <prefix>**{command_name}** <safe/any> [exact tags separated by space]
+        **Cooldown:** 5 seconds per 1 use (member)
+        **Example 1:** {prefix}{command_name} safe blush long_hair
+        **Example 2:** {prefix}{command_name} any
 
         **You need:** None.
         **I need:** `Send Messages`.
         '''
 
-        async with ctx.typing():
-            user_tag_str = "+"
-            chosen_entry = None
-
-            loop = 0
-
-            # Transform user_tag_list into search query.
-            user_tag_str = user_tag_str.join(tags.split())
-
-            while chosen_entry is None:
-                loop += 1
-                async with aiohttp.ClientSession() as session:
-
-                    # Don't set these two too high, otherwise it'll be significantly slow if query with tags.
-                    page = random.randint(1, 20)
-                    count = random.randint(1, 30)
-                    
-                    async with session.get("http://konachan.net/post.json?limit=%d&page=%d&tags=%s" % (count, page, user_tag_str)) as resp:
-                        if resp.status == 200:
-                            query = await resp.json()
-
-                            def filter(entry : dict) -> bool:
-                                if entry["rating"] != "s":
-                                    return False
-                                
-                                RESTRICTED_TAGS = ["breasts", "nipples", "panties", "bikini"] # Expand this
-
-                                for index, tag in enumerate(RESTRICTED_TAGS):
-                                    if tag in entry["tags"]:
-                                        self.bot.debug("Index %d has tag %s so it's not safe." % (index, tag))
-                                        return False
-                                
-                                return True
-                            
-                            # The JSON returned is [{...}, {...}] so we can shuffle it and select index.
-                            random.shuffle(query)
-                            chosen_entry = discord.utils.find(filter, query)
-
-                            if chosen_entry is not None:
-                                self.bot.debug("JSON: " + str(chosen_entry))
-                        else:
-                            print(resp.status)
-                            break
-                self.bot.debug(loop)
-
-            embed = Facility.get_default_embed(
-                title = "Image incoming",
-                description = "Rating: **%s**" % chosen_entry["rating"],
-                url = chosen_entry["file_url"],
-                timestamp = datetime.datetime.utcnow(),
-            )
-            if chosen_entry["file_url"] is not None:
-                embed.set_image(
-                    url = chosen_entry["file_url"]
-                ).set_footer(
-                    text = "Click the title for full image."
-                )
-
-                tag_str = ""
-                for tag in chosen_entry["tags"].split():
-                    tag_str += f"`{tag}` "
-                embed.add_field(name = "Tags", value = tag_str, inline = False)
+        if ctx.invoked_subcommand == None:
+            SEARCH_ENDPOINT = "https://konachan.net/post.json"
+            if img_type.upper() != "SAFE" and img_type.upper() != "ANY":
+                raise commands.BadArgument
+            elif img_type.upper() == "SAFE":
+                SEARCH_ENDPOINT = "https://konachan.net/post.json"
             else:
-                embed.description = "Oops, no image for you. This is usually due to server ratelimit, no image found or \
-                    the image has been reviewed by the FBI and it did not pass (yes the images are usually reviewed by the FBI before sending)."
-                embed.set_image(url = "https://i.imgflip.com/3ddefb.jpg")
-        
-        await ctx.send(embed = embed)
+                SEARCH_ENDPOINT = "https://konachan.com/post.json"
+                
+            async with ctx.typing():
+                user_tag_str = "+"
+                chosen_entry = None
+                cache_entry = None
+
+                loop = 0
+
+                # Transform user_tag_list into search query.
+                user_tag_str = user_tag_str.join(tags.split())
+
+                while chosen_entry is None:
+                    if loop >= 6:
+                        break
+                    async with aiohttp.ClientSession() as session:
+
+                        # Don't set count too high, otherwise it'll be significantly slow if query with tags.
+                        # BUG: If a tag has too few pages, it'll most likely return no image.
+                        # Possible fix: Try cache the first page to see if it has any images, and then query like normal.
+                        # If it's None, then use the cache to display image.
+                        page = random.randint(1, 100)
+                        count = random.randint(1, 10)
+
+                        query_location = SEARCH_ENDPOINT + "?limit=%d&tags=%s&page=%d" % (count, user_tag_str, 0 if loop == 0 else page)
+                        
+                        async with session.get(query_location) as resp:
+                            if resp.status == 200:
+                                # query is a list of dictionary.
+                                query = await resp.json()
+
+                                # Either tag not found, or page too large
+                                if len(query) == 0:
+                                    pass
+
+                                def filter(entry : dict) -> bool:
+                                    #if entry["rating"] != "s":
+                                    #    return False
+                                    #
+                                    #RESTRICTED_TAGS = ["breasts", "nipples", "panties", "bikini"] # Expand this
+                                    #
+                                    #for index, tag in enumerate(RESTRICTED_TAGS):
+                                    #    if tag in entry["tags"]:
+                                    #        self.bot.debug("Index %d has tag %s so it's not safe." % (index, tag))
+                                    #        return False
+                                    #
+                                    #return True
+                                    return True
+                                
+                                random.shuffle(query)
+                                chosen_entry = discord.utils.find(filter, query)
+
+                                if chosen_entry is not None:
+                                    # page != 0 is to avoid selection bias.
+                                    # Basically we cache the first iteration and skip to the next iteration.
+                                    if loop == 0 and page != 0:
+                                        cache_entry = chosen_entry
+                                        chosen_entry = None
+                            else:
+                                print(resp.status)
+                                break
+                    loop += 1
+
+                if chosen_entry is not None or (cache_entry is not None and chosen_entry is None):
+                    chosen_entry = cache_entry if chosen_entry is None else chosen_entry
+                    embed = Facility.get_default_embed(
+                        title = "Image incoming",
+                        description = "Rating: **%s**" % chosen_entry["rating"],
+                        url = chosen_entry["file_url"],
+                        timestamp = datetime.datetime.utcnow(),
+                    )
+                    embed.set_image(
+                        url = chosen_entry["file_url"] if chosen_entry["file_url"] is not None else chosen_entry["preview_url"]
+                    ).set_footer(
+                        text = "Click the title for full image."
+                    )
+
+                    tag_str = ""
+                    for tag in chosen_entry["tags"].split():
+                        tag_str += f"`{tag}` "
+                    embed.add_field(name = "Tags", value = tag_str, inline = False)
+                else:
+                    embed = Facility.get_default_embed(
+                        title = "Uh oh",
+                        timestamp = datetime.datetime.utcnow()
+                    )
+                    embed.description = "Oops, no image for you. This is usually due to no image found or \
+                        the image has been reviewed by the FBI and it did not pass (yes the images are usually reviewed by the FBI before sending)."
+                    embed.set_image(url = "https://i.imgflip.com/3ddefb.jpg")
+            
+                await ctx.send(embed = embed)
+    @konachan.command()
+    async def tags(self, ctx):
+        async with ctx.typing():
+            SEARCH_ENDPOINT = "https://konachan.com/tag.json"
+            query_location = SEARCH_ENDPOINT + "?order=count&limit=10"
+            async with aiohttp.ClientSession() as session:
+                async with session.get(query_location) as resp:
+                    if resp.status == 200:
+                        # query is a list(dictionary)
+                        query = await resp.json()
+                        tag_list = ["`%s`" % tag_object["name"] for tag_object in query]
+
+                        embed = Facility.get_default_embed(
+                            title = "Top 10 Popular Tags",
+                            description = Facility.striplist(tag_list),
+                            timestamp = datetime.datetime.utcnow(),
+                            author = ctx.author
+                        )
+
+                        await ctx.send(embed = embed)
 
     @commands.command()
     @commands.bot_has_permissions(send_messages = True)
