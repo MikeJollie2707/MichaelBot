c49ccdf513d879e223cceb37d97d0b54736b6f61
MikeJollie2707 <bachngo@yahoo.com>
2021-02-11 18:54:58 -0800

    Added troll command (don't use this, seriously)

--- categories/experiment.py
+++ categories/experiment.py
@@ -1,3 +1,4 @@
+import asyncio
 import discord
 from discord.ext import commands
 
@@ -67,6 +68,32 @@ class Experiment(commands.Cog, command_attrs = {"hidden" : True}):
             icon_url = ctx.author.avatar_url
         )
         await ctx.send(embed = embed)
+
+    @commands.command()
+    async def wake_up_shiro(self, ctx):
+        '''
+        A fun way to wake up <@391582107446804485> due to his shitty mobile notification.
+        '''
+        while True:
+            messages = [
+                "<@391582107446804485> wake up.",
+                "<@391582107446804485> hello.",
+                "<@391582107446804485> is washing the dishes.",
+                "<@391582107446804485> how many pings now?",
+                "<@391582107446804485> imagine not using discord 24/7. Couldn't be me.",
+                "<@391582107446804485> hey hey hey.",
+                "<@391582107446804485> friendly reminder: send a message in discord to stop this madness.",
+                "<@391582107446804485> forgot to send message in discord.",
+                "<@391582107446804485>'s laptop broke, so he can't reply. RIP.",
+                "<@391582107446804485> EEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEE.",
+                "<@391582107446804485> I think your phone is trash, let's use nokia brick instead.",
+                "<@391582107446804485> what's up bro?",
+                "<@391582107446804485> I think I should creep into your DM..."
+            ]
+            import random
+            choose = random.randint(0, len(messages) - 1)
+            await ctx.send(messages[choose])
+            await asyncio.sleep(10)
 
 
     
