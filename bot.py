8404aa7d3049f09f61b0cd15181794e4e8330c06
MikeJollie2707 <bachngo@yahoo.com>
2021-02-11 18:53:49 -0800

    Make logging quiet now work properly

--- bot.py
+++ bot.py
@@ -55,12 +55,13 @@ def setup(bot_name):
     return (bot_info, secrets)
 
 def setupLogger(enable : bool = True):
-    logger = logging.getLogger("discord")
-    logger.setLevel(logging.INFO)
-    handler = logging.FileHandler(filename = "discord.log", encoding = "utf-8", mode = "w")
-    handler.setFormatter(logging.Formatter("%(asctime)s:%(levelname)s:%(name)s: %(message)s"))
-    logger.addHandler(handler)
-
+    if enable:
+        logger = logging.getLogger("discord")
+        logger.setLevel(logging.INFO)
+        handler = logging.FileHandler(filename = "discord.log", encoding = "utf-8", mode = "w")
+        handler.setFormatter(logging.Formatter("%(asctime)s:%(levelname)s:%(name)s: %(message)s"))
+        logger.addHandler(handler)
+    
 if __name__ == "__main__":
     bot_info = secrets = None
     argc = len(sys.argv)
