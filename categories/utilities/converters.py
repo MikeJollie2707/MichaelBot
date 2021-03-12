316e1d0c8f8f8a7e22ee698621da270f6392984f
MikeJollie2707 <bachngo@yahoo.com>
2021-03-11 18:23:37 -0800

    Added a command converter from normal name to id

--- categories/utilities/converters.py
+++ categories/utilities/converters.py
@@ -4,7 +4,14 @@ from discord.ext import commands
 import datetime
 
 import pytimeparse
+import categories.utilities.db as DB
 
 class IntervalConverter(commands.Converter):
     async def convert(self, ctx, arg : str) -> datetime.timedelta:
         return datetime.timedelta(seconds = pytimeparse.parse(arg))
+
+class ItemConverter(commands.Converter):
+    async def convert(self, ctx, arg : str) -> str:
+        async with ctx.bot.pool.acquire() as conn:
+            arg = arg.lower()
+            return await DB.Items.get_internal_name(conn, arg)
