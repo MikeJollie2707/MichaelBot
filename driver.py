a0ade5f6b2c8ce6d4d9955bf03888ad46992a6e6
MikeJollie2707 <bachngo@yahoo.com>
2021-05-11 14:04:58 -0700

    A driver code to test probability

---
+++ driver.py
@@ -0,0 +1,38 @@
+import random
+import utilities.loot as Loot
+
+PICK_TYPE = "diamond_pickaxe"
+SELECT_ITEM = "debris"
+SIMULATION = 1000000
+
+roll_total = 0
+item_total = 0
+
+r = Loot.get_mine_loot(PICK_TYPE, 1)
+rolls = r.pop("rolls")
+reward = {}
+
+for i in range(0, SIMULATION):
+    for item in r:
+        if reward.get(item) is None:
+            reward[item] = 0
+        for j in range(0, rolls):
+            rng = random.random()
+            if rng <= r[item]:
+                reward[item] += 1
+                item_total += 1
+
+text = f'''
+SIMULATION ENDED
+================
+Times: {SIMULATION}
+Pickaxe type: {PICK_TYPE}
+Reported item: {SELECT_ITEM}
+
+Here is the full result: {reward}
+In {SIMULATION} simulations, the number of items are {item_total}.
+The ratio of {SELECT_ITEM} to total is {reward[SELECT_ITEM]} : {item_total}.
+In terms of decimal, this is {(reward[SELECT_ITEM] / item_total) :.5f}, or {reward[SELECT_ITEM] / item_total * 100 :.2f}%.
+'''
+
+print(text)
