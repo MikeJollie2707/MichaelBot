d1655555abce410aa80dd670279e8f8c5a211b74
MikeJollie2707 <bachngo@yahoo.com>
2021-05-13 09:27:55 -0700

    Added a way for `Pages` to be in `Option`

--- templates/menu.py
+++ templates/menu.py
@@ -6,166 +6,6 @@ import typing
 
 from templates.navigate import MinimalPages
 
-class Menu:
-    def __init__(self, init_page : discord.Embed, terminate_emoji : str, return_emoji : str):
-        """
-        Param:
-        - `init_page`: The center page.
-        - `terminate_emoji`: The stop emoji.
-        - `return_emoji`: The return emoji.
-        Exception:
-        - TypeError: This exception is raised when `init_page` is not a `discord.Embed`.
-        """
-        self.__pages__ = {} # A dict with the format {emoji: discord.Embed}
-        self.__terminator__ = terminate_emoji
-        self.__return__ = return_emoji
-
-        if isinstance(init_page, discord.Embed):
-            self.__init_page__ = init_page
-        else:
-            raise TypeError("'init_page' must be discord.Embed.")
-    
-    def add_page(self, emoji : str, page : discord.Embed):
-        """
-        Add page to the menu.
-
-        Param:
-        - `emoji`: the emoji you want the page to be reacted.
-        - `page`: the page you want to add. It must be a `discord.Embed`.
-
-        Exception:
-        - `IndexError`: When `emoji` is the same as `terminate_emoji` in the constructor.
-        - `TypeError`: When `page` is not a `discord.Embed`.
-        """
-        if isinstance(page, discord.Embed):
-            if emoji != self.__terminator__:
-                self.__pages__[emoji] = page
-            else:
-                raise IndexError("Cannot add page with %s emoji." % emoji)
-        else:
-            raise TypeError("'page' must be discord.Embed.")
-    def add_pages(self, pages : typing.Dict[str, discord.Embed]):
-        """
-        Add pages to the menu.
-
-        Param:
-        - `pages`: A dictionary in the format Dict[str, discord.Embed].
-
-        Exception:
-        - `IndexError`: When `emoji` is the same as `terminate_emoji` in the constructor.
-        - `TypeError`: When `page` is not a `discord.Embed`.
-        """
-        for page in pages:
-            self.add_page(page["emoji"], page["page"])
-
-    async def event(self, ctx : commands.Context, channel : discord.TextChannel = None, interupt = True):
-        """
-        This function is a coroutine.
-
-        A function use to interact with the menu.
-
-        Param:
-        - `ctx`: The context.
-        - `channel`: The channel you want to send the menu in. If none provided, it'll use `ctx.channel`.
-        - `interupt`: `False` if you don't want other user to react the menu, `True` otherwise. Default value is `True`.
-        
-        Exception:
-        - `AttributeError`: When the parameter(s) is wrong type.
-        - `discord.Forbidden`: When the bot doesn't have permission to send messages/add reactions/read messages history.
-        """
-
-        bot = ctx.bot
-        channel = ctx.channel if channel is None else channel
-        author = ctx.author
-
-        if len(self.__pages__) == 0:
-            return
-        
-        self.__pages__[self.__terminator__] = None
-
-        message = await channel.send(embed = self.__init_page__)
-        for emoji in self.__pages__:
-            await message.add_reaction(emoji)
-        
-        available_options = [] # Available reactions that a user can react.
-        for key in self.__pages__:
-            available_options.append(key)
-
-        def reaction_check(reaction, user):
-            return reaction.message.id == message.id and user != message.author
-        
-        while True:
-            try:
-                reaction, user = await bot.wait_for("reaction_add", check = reaction_check, timeout = 120.0)
-            except asyncio.TimeoutError:
-                await message.clear_reactions()
-                await message.add_reaction('🕛')
-
-                break
-            else:
-                if interupt == False:
-                    if user == author:
-                        if reaction.emoji in available_options:
-                            if reaction.emoji == self.__terminator__:
-                                await message.edit(content = ":white_check_mark:", embed = None)
-                                await message.clear_reactions()
-                                break
-
-                            elif reaction.emoji == self.__return__:
-                                await message.edit(embed = self.__init_page__)
-
-                                available_options = []
-                                for key in self.__pages__:
-                                    available_options.append(key)
-                                
-                            else:
-                                available_options = []
-                                available_options = self.__return__
-                            
-                                await message.edit(embed = self.__pages__[reaction.emoji])
-                            
-                            await message.clear_reactions()
-                            for emoji in available_options:
-                                await message.add_reaction(emoji)
-                            continue
-                        #else:
-                        #    await message.remove_reaction(reaction, user)
-                    #else: # If uncomment then 1 tab the next line
-                    await message.remove_reaction(reaction, user)
-                else:
-                    if reaction.emoji in available_options:
-                        if reaction.emoji == self.__terminator__:
-                            await message.edit(content = ":white_check_mark:", embed = None)
-                            await message.clear_reactions()
-                            break
-
-                        elif reaction.emoji == self.__return__:
-                            await message.edit(embed = self.__init_page__)
-
-                            available_options = []
-                            for key in self.__pages__:
-                                available_options.append(key)
-                        
-                        else:
-                            available_options = []
-                            available_options = self.__return__
-
-                            await message.edit(embed = self.__pages__[reaction.emoji])
-                        
-                        await message.clear_reactions()
-                        for emoji in available_options:
-                            await message.add_reaction(emoji)
-                        continue
-                    else:
-                        await message.remove_reaction(reaction, user)
-
-class MenuMinimalPages(MinimalPages):
-    def __init__(self, init_page = 0, **kwargs):
-        super().__init__(init_page, **kwargs)
-    
-    async def _on_terminate(self, message):
-        pass
-
 class Option:
     def __init__(self, options, terminate_emoji = '❌', return_emoji = '🔼'):
         """
@@ -173,6 +13,7 @@ class Option:
 
         Parameters:
         - `option`: A recursive list(dict) in the following format: `[base_embed, {emote: [embed, {...}], ...}]`.
+            - To include paginator into the menu, the list (not the base list) must be all `discord.Embed` instead of another `dict`.
         - `terminate_emoji`: The emoji used to stop the menu. **IT MUST NOT BE AN EMOJI IN `option`**.
         - `return_emoji`: The emoji used to return to the previous page. **IT MUST NOT BE AN EMOJI IN `option`**.
         """
@@ -181,7 +22,7 @@ class Option:
         self.__terminate_emoji__ = terminate_emoji
         self.__return_emoji__ = return_emoji
 
-        # This has a recursive format of [embed, {emote: [embed, {...}], ...}]
+        # Track all current reactions except the terminate and return one.
         self.__dynamic_options__ = options[1]
         # A stack-like list to track the previously available emoji for a certain page.
         self.__previous_options__ = []
@@ -218,6 +59,7 @@ class Option:
                     # Terminate/return emoji doesn't belong to choice list, so I can have a check here.
                     option = self.__dynamic_options__.get(str(reaction.emoji))
                     if option is not None:
+                        # Deadend MenuPage.
                         if isinstance(option, discord.Embed):
                             await message.edit(embed = option)
                             await message.clear_reactions()
@@ -226,10 +68,12 @@ class Option:
                             self.__previous_options__.append(self.__dynamic_options__)
                             self.__embeds_route__.append(option)
                             self.__dynamic_options__ = {}
+                        # Either another submenu, or a list of pages.
                         elif isinstance(option, list):
                             await message.edit(embed = option[0])
 
                             await message.clear_reactions()
+                            # Submenu
                             if isinstance(option[1], dict):
                                 for sub_reactions in option[1]:
                                     await message.add_reaction(sub_reactions)
@@ -239,16 +83,20 @@ class Option:
                                 self.__previous_options__.append(self.__dynamic_options__)
                                 self.__embeds_route__.append(option[0])
                                 self.__dynamic_options__ = option[1]
+                            # List of pages, deadend
                             elif isinstance(option[1], discord.Embed):
-                                subpages = MenuMinimalPages(previous = '◀️', forward = '▶️', terminate = self.__return_emoji__)
+                                # We want the terminate to be menu's return emoji to be consistent.
+                                subpages = MinimalPages(previous = '◀️', forward = '▶️', terminate = self.__return_emoji__)
                                 for page in option:
                                     subpages.add_page(page)
-                                
-                                await subpages.start(ctx, message = message)
 
+                                # The whole thing is just one big deadend, so this part here will look like deadend embed part.
                                 self.__previous_options__.append(self.__dynamic_options__)
                                 self.__embeds_route__.append(option[0])
                                 self.__dynamic_options__ = {}
+
+                                await subpages.start(ctx, message = message)
+                                # We "return" from the deadend embed, so go back.
                                 await self._on_return(message)
                     else:
                         # It still needs to check if it's truly the terminate/return emoji or just
--- templates/navigate.py
+++ templates/navigate.py
@@ -7,25 +7,42 @@ from discord.ext import commands
 # A class that support navigating pages of embeds.
 
 class Pages:
+    """
+    A paginator that allows using Discord reactions to switch pages in an ordered manner.
+    
+    By default, there are 5 functionalities:
+    - `fast_previous`: Return to page 0.
+    - `previous`: Return to previous page if possible.
+    - `forward`: Move 1 page forward if possible.
+    - `fast_forward`: Move to the last page.
+    - `terminate`: Exist the paginator.
+    
+    You can customize the emojis for each functionality in the constructor.
+    To remove a functionality, override `_emoji_list` and override `_on_reaction()`.
+    """
     def __init__(self, init_page = 0, **kwargs):
         """
-        Param:
+        Construct a paginator.
+
+        Parameters:
         - `init_page`: The starting page index. Default is `0`.
+        - `fast_previous`, `previous`, `forward`, `fast_forward`, `terminate`: The respective emoji for each functionality. These are all optionals.
         """
         self.__page_list__ = []
-        self.__current_page__ = init_page
+        self._current_page = init_page
 
-        self._FAST_PREVIOUS = kwargs.get("fast_previous")
-        self._PREVIOUS = kwargs.get("previous")
-        self._FORWARD = kwargs.get("forward")
-        self._FAST_FORWARD = kwargs.get("fast_forward")
-        self._TERMINATE = kwargs.get("terminate")
-        self.__emoji_list__ = [self._FAST_PREVIOUS, self._PREVIOUS, self._FORWARD, self._FAST_FORWARD, self._TERMINATE]
+        self._FAST_PREVIOUS = kwargs.get("fast_previous") if kwargs.get("fast_previous") is not None else '⏮️'
+        self._PREVIOUS = kwargs.get("previous") if kwargs.get("previous") is not None else '◀️'
+        self._FORWARD = kwargs.get("forward") if kwargs.get("forward") is not None else '▶️'
+        self._FAST_FORWARD = kwargs.get("fast_forward") if kwargs.get("fast_forward") is not None else '⏭️'
+        self._TERMINATE = kwargs.get("terminate") if kwargs.get("terminate") is not None else '⏹️'
+        self._emoji_list = [self._FAST_PREVIOUS, self._PREVIOUS, self._FORWARD, self._FAST_FORWARD, self._TERMINATE]
 
     def add_page(self, page : discord.Embed):
         """
         Add a page into the paginator.
-        Param:
+        
+        Parameter:
         - `page`: A `discord.Embed`.
 
         Exception:
@@ -40,10 +57,11 @@ class Pages:
         """
         This function is a coroutine.
 
-        A function use to interact with the paginator.
+        A function use to start the paginator.
 
-        Param:
+        Parameter:
         - `ctx`: The context.
+        - `message`: The message to bind to. If none provided, a new message will be sent. Otherwise, it'll edit the message.
         - `channel`: The channel you want the pages to be sent. If none provided, it'll use `ctx.channel`.
         - `interupt`: `False` if you don't want other user to react the paginator, `True` otherwise. Default value is `False`.
         
@@ -67,10 +85,10 @@ class Pages:
             self.__page_list__[num].set_footer(text = "Page %d/%d" % (num + 1, len(self.__page_list__)))
 
         if message is None:
-            message = await channel.send(embed = self.__page_list__[self.__current_page__])
+            message = await channel.send(embed = self.__page_list__[self._current_page])
         else:
-            await message.edit(embed = self.__page_list__[self.__current_page__])
-        for emoji in self.__emoji_list__:
+            await message.edit(embed = self.__page_list__[self._current_page])
+        for emoji in self._emoji_list:
             await message.add_reaction(emoji)
 
         def reaction_check(reaction, user):
@@ -80,59 +98,80 @@ class Pages:
             try:
                 reaction, user = await bot.wait_for("reaction_add", check = reaction_check, timeout = 120.0)
             except asyncio.TimeoutError:
-                self.__current_page__ = -1
+                self._current_page = -1
                 await message.clear_reactions()
                 await message.add_reaction('🕛')
                 break
             else:
                 if interupt or (not interupt and user == author):
-                    if reaction.emoji in self.__emoji_list__:
+                    if reaction.emoji in self._emoji_list:
                         terminate = await self._on_reaction(message, reaction)
                         if terminate:
                             break
                         
-                        await message.edit(embed = self.__page_list__[self.__current_page__])
+                        await message.edit(embed = self.__page_list__[self._current_page])
                 await message.remove_reaction(reaction, user)
     
     async def _on_reaction(self, message, reaction) -> bool:
+        """
+        A method that is called when a reaction is valid (according to `interupt` and author check).
+        Override this if you want remove a functionality.
+
+        Parameter:
+        - `message`: The message the paginator is belonged to.
+        - `reaction`: The reaction.
+
+        Return:
+        - `True` if the paginator is reacted with termination, `False` otherwise.
+        """
         if reaction.emoji == '⏮️':
-            self.__current_page__ = 0
+            self._current_page = 0
         
         elif reaction.emoji == '◀️':
-            if self.__current_page__ != 0:
-                self.__current_page__ -= 1
+            if self._current_page != 0:
+                self._current_page -= 1
 
         elif reaction.emoji == '▶️':
-            if self.__current_page__ < len(self.__page_list__) - 1:
-                self.__current_page__ += 1
+            if self._current_page < len(self.__page_list__) - 1:
+                self._current_page += 1
 
         elif reaction.emoji == '⏭️':
-            self.__current_page__ = len(self.__page_list__) - 1
+            self._current_page = len(self.__page_list__) - 1
             
         elif reaction.emoji == '⏹️':
-            self.__current_page__ = -1
+            self._current_page = -1
             await self._on_terminate(message)    
             return True
         
         return False
     
     async def _on_terminate(self, message):
+        """
+        A cleanup function when the paginator is terminated.
+
+        Parameter:
+        - `message`: The message the paginator is belonged to.
+        """
         await message.edit(content = ":white_check_mark:", embed = None)
         await message.clear_reactions()
 
 class MinimalPages(Pages):
+    """
+    A minimal paginator which only has `forward`, `previous`, and `terminate` functionality.
+    It also silently terminate.
+    """
     def __init__(self, init_page = 0, **kwargs):
         super().__init__(init_page, **kwargs)
 
-        self.__emoji_list__ = [self._PREVIOUS, self._FORWARD, self._TERMINATE]
+        self._emoji_list = [self._PREVIOUS, self._FORWARD, self._TERMINATE]
     
     async def _on_reaction(self, message, reaction) -> bool:
         if reaction.emoji == self._PREVIOUS:
-            if self.__current_page__ != 0:
-                self.__current_page__ -= 1
+            if self._current_page != 0:
+                self._current_page -= 1
         elif reaction.emoji == self._FORWARD:
-            if self.__current_page__ < len(self.__page_list__) - 1:
-                self.__current_page__ += 1
+            if self._current_page < len(self.__page_list__) - 1:
+                self._current_page += 1
         elif reaction.emoji == self._TERMINATE:
             await self._on_terminate(message)
             return True
@@ -140,5 +179,4 @@ class MinimalPages(Pages):
         return False
     
     async def _on_terminate(self, message):
-        await message.edit(content = ":white_check_mark:", embed = None)
-        await message.clear_reactions()
+        pass
