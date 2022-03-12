05b29e755e2790dbb625f13c9b6e17c802bd04a3
MikeJollie2707 <bachngo@yahoo.com>
2022-03-12 01:11:28 -0800

    Added `blacklist_guild`

--- categories/admin.py
+++ categories/admin.py
@@ -1,13 +1,26 @@
 import lightbulb
 import hikari
+import asyncpg
 
 import utilities.helpers as helpers
 import utilities.checks as checks
+import utilities.psql as psql
 from utilities.checks import is_dev
 
 plugin = lightbulb.Plugin("Secret", "Developer-only commands.", include_datastore = True)
 plugin.d.emote = helpers.get_emote(":computer:")
 plugin.add_checks(is_dev, checks.is_guild_enabled, lightbulb.bot_has_guild_permissions(*helpers.COMMAND_STANDARD_PERMISSIONS))
+
+@plugin.command()
+@lightbulb.option("guild_id", "Guild ID to blacklist.")
+@lightbulb.command("blacklist_guild", "Blacklist a guild.", hidden = True)
+@lightbulb.implements(lightbulb.PrefixCommand, lightbulb.SlashCommand)
+async def blacklist_guild(ctx: lightbulb.Context):
+    async with ctx.bot.d.pool.acquire() as conn:
+        async with conn.transaction():
+            await psql.guilds.update_column(conn, ctx.options.guild_id, "is_whitelist", False)
+    
+    await ctx.respond("Blacklisted!", reply = True)
 
 @plugin.command()
 @lightbulb.option("extension", "The extension to reload.")
@@ -28,6 +41,14 @@ async def reload_error(event: lightbulb.CommandErrorEvent):
 async def shutdown(ctx: lightbulb.Context):
     await ctx.respond("Bot shutting down...")
     await ctx.bot.close()
+
+@plugin.command()
+@lightbulb.command("test", "test")
+@lightbulb.implements(lightbulb.PrefixCommand)
+async def test(ctx: lightbulb.Context):
+    async with ctx.bot.d.pool.acquire() as conn:
+        guild = await psql.guilds.get_one(conn, ctx.guild_id)
+        await psql.guilds.update_column(conn, ctx.guild_id, "prefix", "%")
 
 def load(bot):
     bot.add_plugin(plugin)
