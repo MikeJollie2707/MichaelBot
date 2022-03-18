ed79c3d52d6d355aeddab67738d03f66714a3d19
MikeJollie2707 <bachngo@yahoo.com>
2022-03-17 23:05:57 -0700

    Used cache for checking

--- utilities/checks.py
+++ utilities/checks.py
@@ -1,6 +1,8 @@
 import lightbulb
 import hikari
 
+import utilities.helpers as helpers
+import utilities.models as models
 import utilities.errors as errors
 import utilities.psql as psql
 
@@ -11,12 +13,31 @@ def is_dev(ctx: lightbulb.Context) -> bool:
 
 @lightbulb.Check
 async def is_command_enabled(ctx: lightbulb.Context) -> bool:
-    async with ctx.bot.d.pool.acquire() as conn:
-        guild = await psql.guilds.get_one(conn, ctx.guild_id)
-        if guild is None:
-            return True
-        
-        if not guild["is_whitelist"]:
-            raise errors.GuildBlacklisted
+    guild_cache = models.get_guild_cache(ctx.bot, ctx.guild_id)
+    user_cache = models.get_user_cache(ctx.bot, ctx.author.id)
+
+    if guild_cache is None:
+        guild_cache = models.GuildCache()
+        async with ctx.bot.d.pool.acquire() as conn:
+            async with conn.transaction():
+                guild = await psql.Guilds.get_one(conn, ctx.guild_id)
+                if guild is None:
+                    await guild_cache.add_guild_module(conn, ctx.get_guild())
+                else:
+                    await guild_cache.force_sync(conn, ctx.author.id)
+    if user_cache is None:
+        user_cache = models.UserCache()
+        async with ctx.bot.d.pool.acquire() as conn:
+            async with conn.transaction():
+                user = await psql.Users.get_one(conn, ctx.author.id)
+                if user is None:
+                    await user_cache.add_user_module(conn, ctx.author)
+                else:
+                    await user_cache.force_sync(conn, ctx.author.id)
+    
+    if not guild_cache.guild_module["is_whitelist"]:
+        raise errors.GuildBlacklisted
+    if not user_cache.user_module["is_whitelist"]:
+        raise errors.UserBlacklisted
         
     return True
