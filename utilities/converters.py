3767079880318c5d31c04476b6e18958c5e2bbbb
MikeJollie2707 <bachngo@yahoo.com>
2022-04-10 18:32:45 -0700

    Minor change

--- utilities/converters.py
+++ utilities/converters.py
@@ -4,6 +4,6 @@ import hikari
 import datetime
 import pytimeparse
 
-class IntervalConverter(lightbulb.converters.base.BaseConverter):
+class IntervalConverter(lightbulb.converters.BaseConverter):
     async def convert(self, arg: str) -> datetime.timedelta:
         return datetime.timedelta(seconds = pytimeparse.parse(arg))
