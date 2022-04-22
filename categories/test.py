49568fe29be090950c1b61764a39203f6ba5690b
MikeJollie2707 <bachngo@yahoo.com>
2022-04-21 19:10:19 -0700

    Minor changes

--- categories/test.py
+++ categories/test.py
@@ -1,20 +1,29 @@
 import lightbulb
 import hikari
+from lightbulb.ext import tasks
+import humanize
 
-import utilities.checks as checks
-import utilities.helpers as helpers
-import utilities.models as models
+import datetime as dt
 
-plugin = lightbulb.Plugin("Experiment", "Sandbox")
+import utils.checks as checks
+import utils.converters as converters
+import utils.helpers as helpers
+import utils.models as models
+import utils.psql as psql
+
+plugin = lightbulb.Plugin("zzzzzExperiment", "Sandbox")
 plugin.add_checks(checks.is_dev, checks.is_command_enabled, lightbulb.bot_has_guild_permissions(*helpers.COMMAND_STANDARD_PERMISSIONS))
 
 @plugin.command()
 @lightbulb.command("test", "test",  hidden = True)
 @lightbulb.implements(lightbulb.PrefixCommand)
 async def test(ctx: lightbulb.Context):
-    raise SyntaxError("Weeeee")
+    bot: models.MichaelBot = ctx.bot
+
+    await ctx.respond("Weeee")
 
 def load(bot: models.MichaelBot):
     bot.add_plugin(plugin)
+    
 def unload(bot: models.MichaelBot):
     bot.remove_plugin(plugin)
