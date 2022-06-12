9b524b183fff6ff099099a7ba9bfb54c41e9d0ec
MikeJollie2707 <bachngo@yahoo.com>
2022-06-12 07:52:38 -0700

    Add psql exceptions (experimental)

--- utils/psql.py
+++ utils/psql.py
@@ -10,6 +10,30 @@ import typing as t
 import asyncpg
 import hikari
 
+class Error(Exception):
+    '''A base error for high-level PostgreSQL operations.'''
+
+class GetError(Error):
+    '''A base error for SELECT operations.
+    
+    Notes
+    -----
+    This should only be raised when `get_x()` is implicitly called.
+    This will NOT be raised if the user explicitly calls `get_x()`.
+    '''
+
+class InsertError(Error):
+    '''A base error for INSERT operations.'''
+
+class DeleteError(Error):
+    '''A base error for DELETE operations.'''
+
+class UpdateError(Error):
+    '''A base error for UPDATE operations.'''
+
+class DuplicateArrayElement(UpdateError):
+    '''Raised when trying to add an element that's already available in a non-duplicate list.'''
+
 def record_to_dict(record: asyncpg.Record, /) -> t.Optional[dict]:
     '''Convert a `Record` into a `dict` or `None` if the object is already `None`.
 
