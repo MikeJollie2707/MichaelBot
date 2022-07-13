7b45de350072c7d9c7f5d35cef0183cd82dfba05
MikeJollie2707 <bachngo@yahoo.com>
2022-07-13 02:20:22 -0700

    Deprecate the old version of DefaultColor

---
+++ deprecated/defaultcolor.py
@@ -0,0 +1,38 @@
+import hikari
+from enum import Enum
+
+class DefaultColor(Enum):
+    '''
+    Store several default colors to use instantly.
+    '''
+
+    teal = hikari.Color(0x1abc9c)
+    dark_teal = hikari.Color(0x11806a)
+    brand_green = hikari.Color(0x57F287)
+    green = hikari.Color(0x2ecc71)
+    dark_green = hikari.Color(0x1f8b4c)
+    blue = hikari.Color(0x3498db)
+    dark_blue = hikari.Color(0x206694)
+    purple = hikari.Color(0x9b59b6)
+    dark_purple = hikari.Color(0x71368a)
+    magenta = hikari.Color(0xe91e63)
+    dark_magenta = hikari.Color(0xad1457)
+    gold = hikari.Color(0xf1c40f)
+    dark_gold = hikari.Color(0xc27c0e)
+    orange = hikari.Color(0xe67e22)
+    dark_orange = hikari.Color(0xa84300)
+    brand_red = hikari.Color(0xED4245)
+    red = hikari.Color(0xe74c3c)
+    dark_red = hikari.Color(0x992d22)
+    lighter_gray = hikari.Color(0x95a5a6)
+    dark_gray = hikari.Color(0x607d8b)
+    light_gray = hikari.Color(0x979c9f)
+    darker_gray = hikari.Color(0x546e7a)
+    og_blurple = hikari.Color(0x7289da)
+    blurple = hikari.Color(0x5865F2)
+    greyple = hikari.Color(0x5865F2)
+    dark_theme = hikari.Color(0x36393F)
+    fuchsia = hikari.Color(0xEB459E)
+    yellow = hikari.Color(0xFEE75C)
+    black = hikari.Color(0x000000)
+    white = hikari.Color(0xFFFFFF)
