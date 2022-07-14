d7ec09198e657c75cdf3ce310ccd588494d1bf72
MikeJollie2707 <bachngo@yahoo.com>
2022-07-14 01:14:45 -0700

    Fix import issue

--- utils/nav/__init__.py
+++ utils/nav/__init__.py
@@ -1,3 +1,3 @@
 from utils.nav.confirm import ConfirmView
-from utils.nav.menu import MenuComponent, ComplexView
+from utils.nav.menu import MenuButton, MenuComponent, ComplexView
 from utils.nav.navigator import ButtonNavigator, ItemListBuilder, run_view
