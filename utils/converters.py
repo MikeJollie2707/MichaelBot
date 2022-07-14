02fb0e831cbd28910c21e1f06edd452278bef4b6
MikeJollie2707 <bachngo@yahoo.com>
2022-07-14 03:37:18 -0700

    Add ItemConverter

--- utils/converters.py
+++ utils/converters.py
@@ -5,7 +5,15 @@ import datetime
 import lightbulb
 import pytimeparse
 
-class IntervalConverter(lightbulb.converters.BaseConverter):
+from utils import models, psql
+
+class IntervalConverter(lightbulb.BaseConverter):
     '''A converter to convert a string into a `datetime.timedelta` object.'''
     async def convert(self, arg: str) -> datetime.timedelta:
         return datetime.timedelta(seconds = pytimeparse.parse(arg))
+
+class ItemConverter(lightbulb.BaseConverter):
+    '''A converter to convert a string into a `psql.Item` object.'''
+    async def convert(self, arg: str) -> psql.Item:
+        bot: models.MichaelBot = self.context.bot
+        return bot.item_cache.get_by_name(arg)
