09cb4a27290f6daf98eecddc0d5095916b821ae7
MikeJollie2707 <bachngo@yahoo.com>
2022-09-05 16:20:11 -0700

    Fix interval bug

--- utils/converters.py
+++ utils/converters.py
@@ -10,7 +10,10 @@ from utils import models, psql
 class IntervalConverter(lightbulb.BaseConverter):
     '''A converter to convert a string into a `datetime.timedelta` object.'''
     async def convert(self, arg: str) -> datetime.timedelta:
-        return datetime.timedelta(seconds = pytimeparse.parse(arg))
+        if (s := pytimeparse.parse(arg)):
+            return datetime.timedelta(seconds = s)
+        
+        raise TypeError(f"'{arg}' cannot be parsed as a time expression.")
 
 class ItemConverter(lightbulb.BaseConverter):
     '''A converter to convert a string into a `psql.Item` object.'''
